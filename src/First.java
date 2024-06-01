e6466fd5d5f80861a8912710dc5e7e9f80a8e12e
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-06-01 16:24:54 +0530

    first commit

---
+++ src/First.java
@@ -0,0 +1,11 @@
+package TEST.TEST;
+public class First {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
